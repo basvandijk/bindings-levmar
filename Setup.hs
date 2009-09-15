df8cf3e252b63a7fd16b347669fbc34ac0bab44d
Roel van Dijk <vandijk.roel@gmail.com>
2009-09-15 12:11:12 +0200

    Custom configure script to detect lapack library (based on the one from hmatrix)
    Ignore-this: 76905a2b95c5821bd0950f9e8327db62
    
    darcs-hash:20090915101112-b004c-7881af3a44e2f09f4d130e523b26c9d811fc37e0.gz

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,6 @@
+#! /usr/bin/env runhaskell
+
 import Distribution.Simple
 
-main = defaultMain
+main :: IO ()
+main = defaultMainWithHooks autoconfUserHooks
---
+++ levmar_lapack_test.c
@@ -0,0 +1,7 @@
+#include "levmar-2.4/misc.h"
+#include "levmar-2.4/Axb.c"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
